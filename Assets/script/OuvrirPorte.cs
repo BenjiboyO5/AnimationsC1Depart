65cf248837b43dc776a973bf6e891c880fb4bfe9
BenjiboyO5 <ben111952@outlook.com>
2023-11-13 11:59:55 -0500

    truc cool

---
+++ Assets/script/OuvrirPorte.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OuvrirPorte : MonoBehaviour
+{
+
+
+    [SerializeField] GameObject _porteDroite;
+    [SerializeField] GameObject _porteGauche;
+    [SerializeField] GameObject _Joueur;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(_porteDroite);
+        Debug.Log(_porteGauche);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player"){
+
+            _porteDroite.GetComponent<Animator>().SetBool("Ouvre",true);
+            _porteGauche.GetComponent<Animator>().SetBool("Ouvre",true);
+
+        }
+    }
+
+
+    
+    private void OnTriggerExit(Collider other)
+    {
+         if(other.tag == "Player"){
+        _porteGauche.GetComponent<Animator>().SetBool("Ouvre",false);
+    }
+    }
+
+
+    }
